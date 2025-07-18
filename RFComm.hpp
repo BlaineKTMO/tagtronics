54b9baf446145bab3b2d5762cbac6895beb37627
BlaineKTMO <blaine.oania@gmail.com>
2025-07-18 11:32:30 -0400

    Working addressable communication

--- RFComm.hpp
+++ RFComm.hpp
@@ -1,40 +1,42 @@
-#ifndef RF_COMM_HPP
-#define RF_COMM_HPP
+#ifndef RFCOMM_HPP
+#define RFCOMM_HPP
 
 #include <RH_RF69.h>
 #include <SPI.h>
 
-// Platform detection for Feather boards
-#if defined(ADAFRUIT_FEATHER_M0) || defined(ADAFRUIT_FEATHER_M0_EXPRESS)
-  #define RFM69_CS    8
-  #define RFM69_INT   3
-  #define RFM69_RST   4
-#else
-  #error "Unsupported board - add pin definitions for your platform"
-#endif
-
+// Packet structure with addressing
+#pragma pack(push, 1)
 struct RF69_Packet {
-  uint8_t sender_id;
-  uint8_t receiver_id;
-  uint8_t command;
-  char payload[RH_RF69_MAX_MESSAGE_LEN - 3]; // Reserve 3 bytes for headers
-  uint8_t crc;
+    uint8_t sender_id;
+    uint8_t receiver_id;
+    uint8_t command;
+    char payload[50];  // Reduced size to fit in RadioHead's max message length
 };
+#pragma pack(pop)
 
 class RF69_Comm {
-  public:
-    RF69_Comm(uint8_t node_id, float frequency = 868.0);
-    bool begin();
-    void send(uint8_t receiver_id, uint8_t command, const char* message);
+public:
+    RF69_Comm(uint8_t node_id, float frequency, uint8_t cs_pin = 8, 
+              uint8_t int_pin = 3, uint8_t rst_pin = 4);
+    
+    bool begin(const uint8_t* sync_words = nullptr, 
+               const char* encryption_key = nullptr);
+    
+    bool send(uint8_t receiver_id, uint8_t command, const char* message);
     void update();
     void set_receive_handler(void (*handler)(RF69_Packet &packet));
+    
+    void enable_debug(bool enable);
+    int16_t get_last_rssi();
 
-  private:
+private:
     RH_RF69 _radio;
     uint8_t _node_id;
     float _frequency;
-    void (*_receive_handler)(RF69_Packet &);
-    uint8_t _calculate_crc(const uint8_t* data, uint8_t len);
+    uint8_t _rst_pin;
+    void (*_receive_handler)(RF69_Packet &packet);
+    bool _debug_enabled;
+    int16_t _last_rssi;
 };
 
-#endif // RF_COMM_HPP
+#endif
