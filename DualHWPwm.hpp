b1a6825e0335397da87e606ee238b4cc46bc48c3
Blaine Oania <83526333+BlaineKTMO@users.noreply.github.com>
2025-07-24 06:35:23 -0400

    Update DualHWPwm.hpp

--- DualHWPwm.hpp
+++ DualHWPwm.hpp
@@ -8,17 +8,19 @@ public:
     DualHardwarePWM(uint8_t pin1, uint8_t pin2);
     
     void begin(uint32_t frequency = 60);
-    void setDutyCycle1(uint8_t percent);
-    void setDutyCycle2(uint8_t percent);
+    void setDutyCycle1(float percent);
+    void setDutyCycle2(float percent);
 
 private:
     uint8_t _pin1, _pin2;
     uint32_t _frequency;
-    Tcc* _timer1;  // Changed from Tc* to Tcc*
-    Tcc* _timer2;  // Changed from Tc* to Tcc*
+    Tcc* _timer1;
+    Tcc* _timer2;
+    uint8_t _cc_channel1;
+    uint8_t _cc_channel2;
     
     void configureTimer(Tcc* timer, uint8_t gclk_id);
-    void setDutyCycle(Tcc* timer, uint8_t cc_channel, uint8_t percent);
+    void setDutyCycle(Tcc* timer, uint8_t cc_channel, float percent);
 };
 
 #endif
